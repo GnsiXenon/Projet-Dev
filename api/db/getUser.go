93afc6fc911f357231378fdbeb7d99eabef1d052
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-23 09:17:08 +0000

    implement methods to login

--- api/db/getUser.go
+++ api/db/getUser.go
@@ -1,8 +1,15 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 func GetUser(dbConn *sql.DB, mail string) (*User, error) {
-	
-	return &User{}, nil
+	row := dbConn.QueryRow("SELECT (id, name, mail, score) FROM User WHERE mail=?", mail)
+	result := User{}
+	if err := row.Scan(&result); err != nil {
+		return nil, fmt.Errorf("row.Scan(&result): %v", err)
+	}
+	return &result, nil
 }
