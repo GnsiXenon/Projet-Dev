70679dd8b7b870383bf442e1784858a261133231
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-23 09:32:20 +0000

    fix implem to get user

--- api/db/getUser.go
+++ api/db/getUser.go
@@ -6,10 +6,15 @@ import (
 )
 
 func GetUser(dbConn *sql.DB, mail string) (*User, error) {
-	row := dbConn.QueryRow("SELECT (id, name, mail, score) FROM User WHERE mail=?", mail)
+	row, err := dbConn.Query("SELECT id, name, mail, score FROM User WHERE mail=?", mail)
+	if err != nil {
+		return nil, fmt.Errorf(`dbConn.Query("SELECT (id, name, mail, score) FROM User WHERE mail=?", mail): %v`, err)
+	}
 	result := User{}
-	if err := row.Scan(&result); err != nil {
-		return nil, fmt.Errorf("row.Scan(&result): %v", err)
+	for row.Next() {
+		if err := row.Scan(&result.Id, &result.Name, &result.Mail, &result.Score); err != nil {
+			return nil, fmt.Errorf("row.Scan(&result.Id, &result.Name, &result.Mail, &result.Score): %v", err)
+		}
 	}
 	return &result, nil
 }
