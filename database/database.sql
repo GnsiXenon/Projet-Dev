177641ddc81185ba64f83ce38e7eda03a17ebad6
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-23 13:31:48 +0000

    update db to add history table

--- database/database.sql
+++ database/database.sql
@@ -11,5 +11,13 @@ CREATE TABLE Challenge (
     flag VARCHAR(150)
 );
 
+CREATE TABLE History (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    id_challenge INT,
+    FOREIGN KEY (id_user) REFERENCES User(id),
+    FOREIGN KEY (id_challenge) REFERENCES Challenge(id)
+);
+
 INSERT INTO Challenge (flag) VALUES ("HaCoeur{G00d_j0b_y0u_f0und_th3_fl4g}");
 INSERT INTO Challenge (flag) VALUES ("HaCoeur{w3_h0p3_y0u_l34rn3d_s0m3th1ng_4nd_th4t_w3b_c4n_b3_fun_853643}");
