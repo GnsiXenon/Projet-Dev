4a73adb560a6d6a473f16164a9ebff1a0a5118c8
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-24 10:49:15 +0000

    redirect to /login if user isn't connected

--- root-you/src/app/page.tsx
+++ root-you/src/app/page.tsx
@@ -1,4 +1,11 @@
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  if (!cookies().get("token")) {
+    redirect("/login")
+  }
+
   return (
     <main >
       
