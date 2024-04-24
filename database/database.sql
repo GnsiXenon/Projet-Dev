c1c03627c7e866421c0ba034d5f7d372350b9674
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-24 17:30:25 +0000

    intervert flag

--- database/database.sql
+++ database/database.sql
@@ -19,5 +19,5 @@ CREATE TABLE History (
     FOREIGN KEY (id_challenge) REFERENCES Challenge(id)
 );
 
-INSERT INTO Challenge (flag) VALUES ("HaCoeur{G00d_j0b_y0u_f0und_th3_fl4g}");
 INSERT INTO Challenge (flag) VALUES ("HaCoeur{w3_h0p3_y0u_l34rn3d_s0m3th1ng_4nd_th4t_w3b_c4n_b3_fun_853643}");
+INSERT INTO Challenge (flag) VALUES ("HaCoeur{G00d_j0b_y0u_f0und_th3_fl4g}");
