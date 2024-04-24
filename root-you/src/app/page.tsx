e1786e543a0547d3881b496100d1338907b909d3
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-24 17:56:25 +0000

    add 2 chall

--- root-you/src/app/page.tsx
+++ root-you/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers"
-import { redirect } from "next/navigation";
+import { redirect } from "next/navigation"
+import Link from "next/link"
 
 export default function Home() {
   if (!cookies().get("token")) {
@@ -8,7 +9,26 @@ export default function Home() {
 
   return (
     <main >
-      
+      <h1 className="text-7xl font-bold">RootYou</h1>
+      <form className="flex flex-col" action="" method="POST">
+        <div className="flex flex-col">
+          <label>Gauntlet</label>
+          <Link href="http://0.0.0.0:5000">Start challenge</Link>
+          <div>
+            <input type="text" id="1" />
+            <input type="submit" value="Submit" />
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <label>FTC</label>
+          <Link href="http://0.0.0.0:5001">Start challenge</Link>
+          <Link href="./ftc.zip">ftc.zip</Link>
+          <div>
+            <input type="text" id="2" />
+            <input type="submit" value="Submit" />
+          </div>
+        </div>
+      </form>
     </main>
   );
 }
