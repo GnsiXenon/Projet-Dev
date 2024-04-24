7f3cbc29cc595723ddcd712154bdaf1038713206
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-24 18:25:09 +0000

    wip

--- root-you/src/app/page.tsx
+++ root-you/src/app/page.tsx
@@ -2,24 +2,38 @@ import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 import Link from "next/link"
 
-export default function Home() {
+var jwt = require('jsonwebtoken')
+
+export default async function Home() {
   if (!cookies().get("token")) {
     redirect("/login")
   }
 
+  let decoded
+
+  try {
+    let token = cookies().get("token")?.value
+    decoded = jwt.verify(token, 'HaCoeur');
+  } catch(err) {
+    redirect("/api/disconnect")
+  }
+
   return (
-    <main >
+    <main>
       <h1 className="text-7xl font-bold">RootYou</h1>
-      <form className="flex flex-col" action="" method="POST">
-        <div className="flex flex-col">
+      <h2>Welcome {decoded.name}</h2>
+      <p>You have {decoded.score} points</p>
+      <Link href="/api/disconnect">Disconnect</Link>
+      <div className="flex flex-col">
+        <form className="flex flex-col" action="/api/submit" method="POST">
           <label>Gauntlet</label>
           <Link href="http://0.0.0.0:5000">Start challenge</Link>
           <div>
             <input type="text" id="1" />
             <input type="submit" value="Submit" />
           </div>
-        </div>
-        <div className="flex flex-col">
+        </form>
+        <form className="flex flex-col" action="/api/submit" method="POST">
           <label>FTC</label>
           <Link href="http://0.0.0.0:5001">Start challenge</Link>
           <Link href="./ftc.zip">ftc.zip</Link>
@@ -27,8 +41,8 @@ export default function Home() {
             <input type="text" id="2" />
             <input type="submit" value="Submit" />
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </main>
   );
 }
