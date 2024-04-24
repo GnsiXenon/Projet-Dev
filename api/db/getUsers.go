3a8235d983ab7fa9651b93c9e1ebf7f852f2b150
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-24 10:17:08 +0000

    implement GetUsers logic

--- api/db/getUsers.go
+++ api/db/getUsers.go
@@ -1,7 +1,20 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 func GetUsers(dbConn *sql.DB) ([]User, error) {
-	return nil, nil
+	rows, err := dbConn.Query("SELECT (id, name, score) FROM User")
+	if err != nil {
+		return nil, fmt.Errorf(`dbConn.Query("SELECT (id, name, score) FROM User"): %v`, err)
+	}
+	result := []User{}
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.Id, &user.Name, &user.Score)
+		result = append(result, user)
+	}
+	return result, nil
 }
