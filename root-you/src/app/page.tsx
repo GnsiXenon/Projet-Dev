4a36608af0adc062da1831e5f601586d42df382a
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-25 14:44:43 +0000

    add more challenge

--- root-you/src/app/page.tsx
+++ root-you/src/app/page.tsx
@@ -53,6 +53,34 @@ export default async function Home() {
             <input type="submit" value="Submit" />
           </div>
         </form>
+        <form className="flex flex-col" action="/api/submit" method="POST">
+          <label>Apero</label>
+          <p>
+            Suite à des activités suspectes dans la région, une enquête approfondie a été lancée pour identifier un espion présumé opérant sous le nom de code 'Marion Sinktaw'.
+            Des rapports récents suggèrent que cette personne pourrait être liée à plusieurs incidents d'espionnage industriel et de cyberintrusions.
+            Les autorités ont déployé des ressources spéciales pour traquer cette mystérieuse figure, tandis que les agences de renseignement internationales ont été alertées pour une coopération accrue dans cette affaire.
+            Avec la sécurité nationale en jeu, chaque indice et témoignage sont cruciaux pour démêler le réseau d'intrigues dans lequel 'Marion Sinktaw' est impliqué.
+            Nous avons besoin de ces deux informations pour mener à bien notre mission : Le nom de la piscine en lien avec son lieu de travail, et le numéro de rue ou elle habite. (Séparer les deux informations par un _)
+          </p>
+          <div>
+            <input type="hidden" name="chall-id" value="3" />
+            <input type="hidden" name="user-id" value={decoded["id-user"]} />
+            <input  type="hidden" name="mail" value={decoded["mail"]} />
+            <input type="text" name="flag" className="text-black" />
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+        <form className="flex flex-col" action="/api/submit" method="POST">
+          <label>Kyk's bot</label>
+          <Link href="https://discord.gg/VD7T4qjKF3">Start challenge</Link>
+          <div>
+            <input type="hidden" name="chall-id" value="4" />
+            <input type="hidden" name="user-id" value={decoded["id-user"]} />
+            <input  type="hidden" name="mail" value={decoded["mail"]} />
+            <input type="text" name="flag" className="text-black" />
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
       </div>
     </main>
   );
