60c285a448b9a51e5bfa86f021311a930c3d44ff
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-28 20:06:07 +0000

    add link to scoreboard

--- root-you/src/app/page.tsx
+++ root-you/src/app/page.tsx
@@ -25,6 +25,7 @@ export default async function Home() {
     <main className="flex flex-col justify-start gap-3">
       <nav className="w-full flex flex-row justify-between items-center h-20 border-b-white border-b-[2px] bg-gray-500/30 backdrop-blur-sm">
         <h1 className="relative left-3 text-7xl font-bold">RootYou</h1>
+        <Link href="/scoreboard">Scoreboard</Link>
         <form action="/api/delete-user" method="POST">
           <input type="hidden" name="mail" value={decoded.mail} />
           <input className="text-red-700" type="submit" value="Delete account" />
