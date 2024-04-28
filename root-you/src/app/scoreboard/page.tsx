a82c35a1e8ea37e3c7b5db1530ff862fe9f16668
Tom Jegou <40003294+TomJegou@users.noreply.github.com>
2024-04-28 20:13:34 +0000

    improve scoreboard front

--- root-you/src/app/scoreboard/page.tsx
+++ root-you/src/app/scoreboard/page.tsx
@@ -13,12 +13,12 @@ export default async function ScoreBoard() {
     }
 
     return (
-        <main className="flex flex-col justify-center items-center gap-4">
-            {data.map((user: {"id-user": string, "name": string, "score": 0}) => (<div className="flex flex-row w-[90vw] justify-between p-4 bg-red-600/50 rounded-lg backdrop-blur-sm" key={user["id-user"]}>
-                <p>
+        <main className="flex flex-col justify-center items-center gap-4 relative top-4">
+            {data.map((user: {"id-user": string, "name": string, "score": 0}) => (<div className="flex flex-row w-[90vw] justify-between p-6 bg-blue-600/50 rounded-lg backdrop-blur-sm" key={user["id-user"]}>
+                <p className="text-3xl">
                     {user["name"]}
                 </p>
-                <p>
+                <p className="text-3xl">
                     {user["score"]}
                 </p>
             </div>))}
